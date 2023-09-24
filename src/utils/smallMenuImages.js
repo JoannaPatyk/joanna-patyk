844585f34ed62158037e46cecd9d85e9b01867cd
Joanna Patyk <patykjoanna93@gmail.com>
2023-09-24 16:33:19 +0200

    Add screenshots and remove old one

--- src/utils/smallMenuImages.js
+++
@@ -1,9 +0,0 @@
-import orangeStarsImage from '../images/orange-stars.png';
-import orangePlantsImage from '../images/orange-plants.png';
-
-const smallMenuImages = [
-    { id: 0, src: orangeStarsImage, className: 'smallMenu-images smallMenu-stars', alt: 'Gwiazdy' },
-    { id: 1, src: orangePlantsImage, className: 'smallMenu-images smallMenu-plants', alt: 'Rośliny' }
-];
-
-export default smallMenuImages;
