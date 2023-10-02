9775fcd67823cc46c4231cfc27a006c82bb90e32
Joanna Patyk <patykjoanna93@gmail.com>
2023-10-02 19:08:50 +0200

    Add mailing service
    Added messaging service, installed EmailJS
    Added the ability to redirect messages and data from the form to a specified email address using EmailJS

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Button({ id, children, onClick, nextClass, type = 'button' }) {
+function Button({ id, children, onClick, nextClass, disabled = false, type = 'button' }) {
     return (
-        <button id={id} type={type} onClick={onClick} className={`basic-btn ${nextClass}`}>
+        <button id={id} type={type} onClick={onClick} className={`basic-btn ${nextClass}`} disabled={disabled}>
             {children}
         </button>
     );
@@ -14,6 +14,7 @@ Button.propTypes = {
     nextClass: PropTypes.string,
     id: PropTypes.string,
     onClick: PropTypes.func,
+    disabled: PropTypes.bool,
     children: PropTypes.node.isRequired
 };
 
