40f747b467c5da855bc8752c6752dc0b94a192c5
Joanna Patyk <patykjoanna93@gmail.com>
2023-10-12 19:21:05 +0200

    Add privacy policy

---
+++ src/utils/privacyText.js
@@ -0,0 +1,33 @@
+const privacyText = [
+    {
+        title: '1. Informacje ogólne',
+        description:
+            'Niniejsza polityka dotyczy strony funkcjonującej pod adresem url: joannapatyk.com. Operatorem serwisu oraz Administratorem danych osobowych jest: Joanna Patyk. Adres kontaktowy poczty elektronicznej operatora: patykjoanna93@gmail.com Operator jest Administratorem Twoich danych osobowych w odniesieniu do danych podanych dobrowolnie w Serwisie. Serwis wykorzystuje dane osobowe w następujących celach: obsługa zapytań przez formularz, realizacja zamówionych usług oraz prezentacja oferty lub informacji. Serwis realizuje funkcje pozyskiwania informacji o użytkownikach i ich zachowaniu w następujący sposób: Poprzez dobrowolnie wprowadzone w formularzach dane, które zostają wprowadzone do systemów Operatora. Poprzez zapisywanie w urządzeniach końcowych plików cookie (tzw. „ciasteczka”).'
+    },
+    {
+        title: '2. Wybrane metody ochrony danych',
+        description:
+            'Miejsca wprowadzania danych osobowych są chronione w warstwie transmisji (certyfikat SSL). Dzięki temu dane osobowe i dane logowania, wprowadzone na stronie, zostają zaszyfrowane w komputerze użytkownika i mogą być odczytane jedynie na docelowym serwerze. Operator okresowo zmienia swoje hasła administracyjne. Istotnym elementem ochrony danych jest regularna aktualizacja wszelkiego oprogramowania, wykorzystywanego przez Operatora do przetwarzania danych osobowych, co w szczególności oznacza regularne aktualizacje komponentów programistycznych.'
+    },
+    {
+        title: '3. Hosting',
+        description: 'Serwis jest hostowany (technicznie utrzymywany) na serwerach operatora: nazwa.pl'
+    },
+    {
+        title: '4. Twoje prawa i dodatkowe informacje o sposobie wykorzystania danych',
+        description:
+            'W niektórych sytuacjach Administrator ma prawo przekazywać Twoje dane osobowe innym odbiorcom, jeśli będzie to niezbędne do wykonania zawartej z Tobą umowy lub do zrealizowania obowiązków ciążących na Administratorze. Dotyczy to takich grup odbiorców: upoważnieni pracownicy i współpracownicy, którzy korzystają z danych w celu realizacji celu działania strony Twoje dane osobowe przetwarzane przez Administratora nie dłużej, niż jest to konieczne do wykonania związanych z nimi czynności określonych osobnymi przepisami (np. o prowadzeniu rachunkowości). W odniesieniu do danych marketingowych dane nie będą przetwarzane dłużej niż przez 3 lata. Przysługuje Ci prawo żądania od Administratora: dostępu do danych osobowych Ciebie dotyczących, ich sprostowania, usunięcia, ograniczenia przetwarzania oraz przenoszenia danych. Przysługuje Ci prawo do złożenia sprzeciwu w zakresie przetwarzania danych osobowych w celu wykonania prawnie uzasadnionych interesów realizowanych przez Administratora, w tym profilowania, przy czym prawo sprzeciwu nie będzie mogło być wykonane w przypadku istnienia ważnych prawnie uzasadnionych podstaw do przetwarzania, nadrzędnych wobec Ciebi interesów, praw i wolności, w szczególności ustalenia, dochodzenia lub obrony roszczeń.'
+    },
+    {
+        title: '5. Informacje w formularzach',
+        description:
+            'Serwis zbiera informacje podane dobrowolnie przez użytkownika, w tym dane osobowe, o ile zostaną one podane. Serwis może zapisać informacje o parametrach połączenia (oznaczenie czasu, adres IP). Serwis, w niektórych wypadkach, może zapisać informację ułatwiającą powiązanie danych w formularzu z adresem e-mail użytkownika wypełniającego formularz. W takim wypadku adres e-mail użytkownika pojawia się wewnątrz adresu url strony zawierającej formularz. Dane podane w formularzu są przetwarzane w celu wynikającym z funkcji konkretnego formularza, w celu dokonania procesu obsługi zgłoszenia serwisowego lub kontaktu handlowego, rejestracji usług itp. Każdorazowo kontekst i opis formularza w czytelny sposób informuje, do czego on służy.'
+    },
+    {
+        title: '6. Informacja o plikach cookies',
+        description:
+            'Serwis korzysta z plików cookies.Pliki cookies (tzw. „ciasteczka”) stanowią dane informatyczne, w szczególności pliki tekstowe, które przechowywane są w urządzeniu końcowym Użytkownika Serwisu i przeznaczone są do korzystania ze stron internetowych Serwisu. Cookies zazwyczaj zawierają nazwę strony internetowej, z której pochodzą, czas przechowywania ich na urządzeniu końcowym oraz unikalny numer. Podmiotem zamieszczającym na urządzeniu końcowym Użytkownika Serwisu pliki cookies oraz uzyskującym do nich dostęp jest operator Serwisu.'
+    }
+];
+
+export default privacyText;
