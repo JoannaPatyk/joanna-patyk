f48a55907f647c2d9bdb0f43479778417362bccf
Joanna Patyk <patykjoanna93@gmail.com>
2023-10-13 13:34:32 +0200

    Added lawyer site

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -30,8 +30,19 @@ import id3 from '../screens/id-3.png';
 import id4 from '../screens/id-4.png';
 import id5 from '../screens/id-5.png';
 
+import ls1 from '../screens/ls-1.png';
+import ls2 from '../screens/ls-2.png';
+import ls3 from '../screens/ls-3.png';
+import ls4 from '../screens/ls-4.png';
+import ls5 from '../screens/ls-5.png';
+import ls6 from '../screens/ls-6.png';
+import ls7 from '../screens/ls-7.png';
+import ls8 from '../screens/ls-8.png';
+import ls9 from '../screens/ls-9.png';
+
 import { FaReact, FaCss3Alt, FaHtml5, FaNodeJs } from 'react-icons/fa';
 import { SiStyledcomponents, SiExpress, SiMongodb } from 'react-icons/si';
+import { FaWordpress } from 'react-icons/fa6';
 import { DiJavascript1 } from 'react-icons/di';
 
 const projects = [
@@ -129,6 +140,60 @@ const projects = [
     {
         id: 1,
         title: [
+            <a href="https://interior-designer-website.onrender.com/" className="link" key={0}>
+                Wizytówka architekta wnętrz
+            </a>
+        ],
+        images: [
+            { id: 1, src: id1, className: 'project-image id-image1', alt: 'Strona główna' },
+            { id: 2, src: id2, className: 'project-image id-image1', alt: 'Slider' },
+            { id: 3, src: id3, className: 'project-image id-image1', alt: 'Zalety' },
+            { id: 4, src: id4, className: 'project-image id-image1', alt: 'Realizacja usługi' },
+            { id: 5, src: id5, className: 'project-image id-image1', alt: 'Kontakt' }
+        ],
+        description: [
+            <h3 key={0}>
+                Stworzyłam przykładowy projekt będący dynamiczną wizytówką dla architekta wnętrz. Dzięki wyjątkowemu
+                minimalistycznemu i czytelnemu układowi, Klienci mogą łatwo przeglądać dostępne usługi oraz zrealizowane
+                projekty.
+            </h3>
+        ],
+        characteristics: [
+            <li key={0}>
+                <span>Przejrzyste menu</span> - przedstawienie menu w sposób czytelny i atrakcyjny wizualnie, tak aby
+                korzystanie ze strony było łatwe dla użytkownika i umożliwiało szybkie przemieszczanie się po stronie.
+            </li>,
+            <li key={1}>
+                <span>Prezentacja projektów</span> - uwzględnienie kluczowej kwestii jaką jest możliwość przeglądania
+                zrealizowanych projektów w formie mini slajderów.
+            </li>,
+            <li key={2}>
+                <span>Logo i dodatkowe informacje</span> - stworzenie logo oraz sekcja "O mnie", które budują zaufanie
+                do firmy i zwiększają jej wiarygodność w oczach potencjalnych klientów.
+            </li>
+        ],
+        technologies: [
+            <div key={0} className="technologie">
+                <FaReact />
+                <p className="react">React</p>
+            </div>,
+            <div key={1} className="technologie">
+                <SiStyledcomponents />
+                <p className="styled">Styled-components</p>
+            </div>,
+            <div key={2} className="technologie">
+                <FaCss3Alt />
+                <p className="css">CSS</p>
+            </div>,
+            <div key={3} className="technologie">
+                <FaHtml5 />
+                <p className="html">HTML</p>
+            </div>
+        ]
+    },
+    {
+        id: 2,
+        title: [
             <a href="https://skill-boost-quality-website.onrender.com/" className="link" key={0}>
                 Skill Boost Quality
             </a>
@@ -195,54 +260,48 @@ const projects = [
     {
         id: 3,
         title: [
-            <a href="https://interior-designer-website.onrender.com/" className="link" key={0}>
-                Wizytówka architekta wnętrz
+            <a href="/projects/" className="link" key={0}>
+                Kancelaria prawna
             </a>
         ],
         images: [
-            { id: 1, src: id1, className: 'project-image id-image1', alt: 'Strona główna' },
-            { id: 2, src: id2, className: 'project-image id-image1', alt: 'Slider' },
-            { id: 3, src: id3, className: 'project-image id-image1', alt: 'Zalety' },
-            { id: 4, src: id4, className: 'project-image id-image1', alt: 'Realizacja usługi' },
-            { id: 5, src: id5, className: 'project-image id-image1', alt: 'Kontakt' }
+            { id: 1, src: ls1, className: 'project-image sbq-image1', alt: 'Strona główna' },
+            { id: 2, src: ls2, className: 'project-image sbq-image1', alt: 'Strona główna z aktywnym menu' },
+            { id: 3, src: ls3, className: 'project-image sbq-image1', alt: 'Motto firmy' },
+            { id: 4, src: ls4, className: 'project-image sbq-image1', alt: 'Formularz kontaktowy' },
+            { id: 5, src: ls5, className: 'project-image sbq-image1', alt: 'Informacje o zakresie usług' },
+            { id: 6, src: ls6, className: 'project-image sbq-image1', alt: 'Informację ze stopki' },
+            { id: 7, src: ls7, className: 'project-image sbq-image1', alt: 'Strona główna z aktywnym przyciskiem' },
+            { id: 8, src: ls8, className: 'project-image sbq-image1', alt: 'Opis jednej z usług' },
+            { id: 9, src: ls9, className: 'project-image sbq-image1', alt: 'Opis jednej z usług' }
         ],
         description: [
             <h3 key={0}>
-                Stworzyłam przykładowy projekt będący dynamiczną wizytówką dla architekta wnętrz. Dzięki wyjątkowemu
-                minimalistycznemu i czytelnemu układowi, Klienci mogą łatwo przeglądać dostępne usługi oraz zrealizowane
-                projekty.
+                Korzystając z systemu zarządzania treścią na stronie, jakim jest Wordpress, stworzyłam przykładowy
+                projekt będący wizytówką dla kancelarii prawnej. Potencjalni klienci mogą łatwo przeglądać dostępne
+                usługi. Witryna zawiera stronę główną z ogólnymi informacjami na temat kancelarii, zakresem usług oraz
+                danymi kontaktowymi. Kolejna zakładka zawiera informacje o pracownikach oraz przykładowe opinie
+                klientów. Na następnej podstronie znajdują się szersze informacje o świadczonych usługach. Ostatnia
+                zakładka została poświęcona danym kontaktowym i zawiera formularz oraz FAQ wraz z danymi kontaktowymi.
             </h3>
         ],
         characteristics: [
             <li key={0}>
-                <span>Przejrzyste menu</span> - przedstawienie menu w sposób czytelny i atrakcyjny wizualnie, tak aby
-                korzystanie ze strony było łatwe dla użytkownika i umożliwiało szybkie przemieszczanie się po stronie.
+                <span>Minimalistyczny design</span> - stron wyróżnia się minimalistycznym designem, który skupia uwagę
+                na istotnych informacjach. Niebieska kolorystyka nadaje stronie profesjonalny i zaufany charakter, co
+                pomaga budować pozytywny wizerunek kancelarii prawnej. Dzięki przejrzystemu układowi i estetyce,
+                użytkownicy łatwo znajdą potrzebne informacje.
             </li>,
             <li key={1}>
-                <span>Prezentacja projektów</span> - uwzględnienie kluczowej kwestii jaką jest możliwość przeglądania
-                zrealizowanych projektów w formie mini slajderów.
-            </li>,
-            <li key={2}>
-                <span>Logo i dodatkowe informacje</span> - stworzenie logo oraz sekcja "O mnie", które budują zaufanie
-                do firmy i zwiększają jej wiarygodność w oczach potencjalnych klientów.
+                <span>Łatwa nawigacja</span> - rojekt strony został stworzony z myślą o prostocie i intuicyjności
+                nawigacji. Oferujemy klientom płynne i szybkie przeglądanie treści, dzięki czemu mogą łatwo znaleźć
+                potrzebne informacje na temat usług prawnych, zespołu prawników oraz danych kontaktowych.
             </li>
         ],
         technologies: [
             <div key={0} className="technologie">
-                <FaReact />
-                <p className="react">React</p>
-            </div>,
-            <div key={1} className="technologie">
-                <SiStyledcomponents />
-                <p className="styled">Styled-components</p>
-            </div>,
-            <div key={2} className="technologie">
-                <FaCss3Alt />
-                <p className="css">CSS</p>
-            </div>,
-            <div key={3} className="technologie">
-                <FaHtml5 />
-                <p className="html">HTML</p>
+                <FaWordpress />
+                <p className="react">WordPress</p>
             </div>
         ]
     }
